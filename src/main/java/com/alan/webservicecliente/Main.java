6223a06a768b2f0ce32bd5e36b9478a300975c95
Alan <alansvela2019@gmail.com>
2025-06-10 09:55:56 -0500

    clase

--- src/main/java/com/alan/webservicecliente/Main.java
+++ src/main/java/com/alan/webservicecliente/Main.java
@@ -1,17 +1,26 @@
 package com.alan.webservicecliente;
 
+import com.alan.servicioweb.servicios.Curso;
+import com.alan.servicioweb.servicios.ServicioWs;
+import com.alan.servicioweb.servicios.ServicioWsImplementService;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //creamos el objeto de servicio web
+        ServicioWs service= new ServicioWsImplementService().getServicioWsImplementPort();
+        //imprimimos en consola el saludo
+        System.out.println("Saludo: "+ service.saludar("Alan Velasco"));
+        //inicializamos el objeto curso
+        Curso curso= new Curso();
+        //añado el nuevo curso
+        curso.setNombre("JavaScript");
+        Curso respuesta= service.crear(curso);
+        //imprimimos el nuevo objeto o el nuevo curso añadido
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Nuevo curso: "+ curso.getNombre());
+        //listamos los cursos
+        service.listar().forEach(c->System.out.println(c.getNombre()));
     }
 }
